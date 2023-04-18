4f45ab648ef3df9f511c699e02aac75b486d71a1
LiuCH <liuch@allinfinance.com>
2023-04-18 14:55:40 +0800

    start

---
+++ src/main/java/com/liu/myvoice/MyVoiceApplication.java
@@ -0,0 +1,13 @@
+package com.liu.myvoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyVoiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyVoiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liu/myvoice/MyVoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liu.myvoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyVoiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
